2f4620f9619b8a831622d0ed40303d1627f6f6d3
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-06-28 20:43:07 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home"
+import Login from "./Login"
+import Signup from "./Signup"
+import Profile from "./Profile"
+import Saved from "./Saved"
+
+export  {Home ,Login , Signup , Profile , Saved}
