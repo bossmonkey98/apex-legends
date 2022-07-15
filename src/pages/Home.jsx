9b222736ef6f9b695ef582155ab0a164954275a5
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-07-16 01:28:45 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useEffect } from 'react'
+import {
+	fetchAllPosts,
+	fetchUserPost,
+	fetchUserFeed,
+	fetchPostWithLimit,
+	deleteUserPost,
+	editUserPost,
+	likeUserPost,
+	dislikeUserPost,
+	createUserPost,
+} from "../services/postService"
+import { useSelector, useDispatch } from 'react-redux'
+import { Loader } from '../components/Loader'
+import PostCard from '../components/post/PostCard'
 
 const Home = () => {
-  return (
-    <div className='home page'>Home</div>
-  )
+	const { allposts, isLoading } = useSelector((store) => store.posts)
+	const dispatch = useDispatch()
+
+	useEffect(
+		() => {
+			setTimeout(() =>
+				dispatch(fetchAllPosts()), 3000)
+		},
+		[]
+	)
+	return (
+		isLoading ? (
+			<div className='home page load'>
+				<Loader />
+			</div>) : (
+			<div className='home page'>
+				{allposts.map(
+					(post) =>
+						<PostCard key={post._id} post={post}
+						/>
+				)}
+			</div>
+		)
+	)
 }
 
 export default Home
